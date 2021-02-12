650d9c032476abb8670274d8025b7891ece8a770
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-02-12 09:48:02 +0200

    Update Main.java

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/Main.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/Main.java
@@ -2,6 +2,6 @@ package IO_00._00_Алещенко_Олексій_Вадимович;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!:)");
+        System.out.println("Hello world!:)2");
     }
 }
