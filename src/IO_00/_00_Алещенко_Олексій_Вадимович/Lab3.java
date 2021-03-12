a90cca522341b91a57ac163c41b02905aca1dc67
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-03-12 10:12:55 +0200

    update lab3

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/Lab3.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/Lab3.java
@@ -1,7 +1,23 @@
 package IO_00._00_Алещенко_Олексій_Вадимович;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Lab3 {
     public static void main(String[] args) {
+        String textString = "Hello world!\nAAA";
+        System.out.println(textString);
+        System.out.println(textString.length());
+        System.out.println(textString.lines().filter(s -> s.length() > 3).count());
+        String[] textStringLines = textString.split("\n");
+        System.out.println(Arrays.toString(textStringLines));
+        System.out.println("\"A\".compareTo(\"A\"): " + ("A".compareTo("A")));
+        System.out.println("\"A\".compareTo(\"B\"): " + ("A".compareTo("B")));
+        System.out.println("\"B\".compareTo(\"A\"): " + ("B".compareTo("A")));
+
+        StringBuilder stringBuilder = new StringBuilder("A");
+        stringBuilder.append("B");
+        System.out.println("stringBuilder = " + stringBuilder);
         /*String string = new String("Done!");
         String string1 = "Done!" + string;
         System.out.println(string);
@@ -28,7 +44,26 @@ public class Lab3 {
         System.out.println("(s == s2) = " + (s == s2));
         System.out.println("s.equals(s2) = " + s.equals(s2));
 
+        String stringTest1 = "AB";
+        /*String stringTest2 = new String("AB");
+        System.out.println("stringTest1 == stringTest2: " + (stringTest1 == stringTest2));*/
+        String stringTest3 = "AB";
+//        stringTest3 += "B";
+        System.out.println("stringTest1 == stringTest3: " + (stringTest1 == stringTest3));
+
         System.out.println(s);
+
+        Integer integer = Integer.valueOf(1);
+        Integer integer2 = Integer.valueOf(1);
+        System.out.println("integer == integer2: " + (integer == integer2));
+
+        /*int[] array = new int[4];
+        ArrayList list = new ArrayList();
+        list.add("A");
+        list.add(1);
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(2);
+        integerArrayList.add("A");*/
 
         System.out.println("Done!");
     }
