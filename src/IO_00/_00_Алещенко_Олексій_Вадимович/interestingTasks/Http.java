6c3afc8040062de59a4fe0ee4b72cf114be7a7c6
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-03-16 15:11:18 +0200

    add interesting task "HTTP"

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/interestingTasks/Http.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/interestingTasks/Http.java
@@ -1,4 +1,8 @@
 package IO_00._00_Алещенко_Олексій_Вадимович.interestingTasks;
 
 public class Http {
+    public static void main(String[] args) {
+        http://google.com
+        System.out.println("Done:)");
+    }
 }
