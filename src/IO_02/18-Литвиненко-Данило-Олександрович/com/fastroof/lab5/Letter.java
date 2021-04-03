1885e7a8f0904b1febe1e5157b8006c2f8a86c55
Fastroof <LytvDan@gmail.com>
2021-04-03 15:07:52 +0300

    Add lab5

---
+++ src/IO_02/18-Литвиненко-Данило-Олександрович/com/fastroof/lab5/Letter.java
@@ -0,0 +1,13 @@
+package com.fastroof.lab5;
+
+public class Letter {
+    private char letterChar;
+
+    public Letter(char letterChar) {
+        this.letterChar = letterChar;
+    }
+
+    public char toChar() {
+        return letterChar;
+    }
+}
