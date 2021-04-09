1e154d349f562004f365a0ba4c970b12595e66fe
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-04-09 10:05:19 +0300

    add lab5

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/lab5/Main.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab5/Main.java
@@ -2,10 +2,20 @@ package IO_00._00_Алещенко_Олексій_Вадимович.lab5;
 
 public class Main {
     public static void main(String[] args) {
+        Letter letter = new Letter('H');
         Word word = new Word("Hello");
         Word word2 = new Word("world");
-        System.out.println(word.toString());
+        Word word3 = new Word(new Letter[]{
+                letter,
+                new Letter('e'),
+                new Letter('l'),
+                new Letter('l'),
+                new Letter('o'),
+        });
+        System.out.println(word/*.toString()*/);
+        System.out.println(word3);
         Sentence sentence = new Sentence(new Word[]{word, word2});
+        Sentence sentence2 = new Sentence("Hello world");
         System.out.println(sentence);
     }
 }
--- src/IO_00/_00_Алещенко_Олексій_Вадимович/lab5/Sentence.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab5/Sentence.java
@@ -3,10 +3,19 @@ package IO_00._00_Алещенко_Олексій_Вадимович.lab5;
 import java.util.Arrays;
 
 public class Sentence {
+    //    todo add punctuation
     private Word[] words;
 
     public Sentence(Word[] words) {
         this.words = words;
+    }
+
+    public Sentence(String sentenceString) {
+        String[] wordStrings = sentenceString.split(" ");
+        words = new Word[wordStrings.length];
+        for (int i = 0; i < wordStrings.length; i++) {
+            words[i] = new Word(wordStrings[i]);
+        }
     }
 
     @Override
@@ -17,6 +26,7 @@ public class Sentence {
                     .append(word.toString())
                     .append(" ");
         }
+        sentenceString.deleteCharAt(sentenceString.length() - 1);
         /*String sentenceString = "";
         for (Word word : words) {
             sentenceString += word.toString() + " ";
