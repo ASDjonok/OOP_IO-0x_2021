255e5b6ab11e1de15d250c7870ad79b7d7ca9051
ic3top <potatai123@gmail.com>
2021-05-17 23:58:20 +0300

    test Lab-3

--- src/IO_02/08-Герчук-Володимир/Lab_3/app.js
+++ src/IO_02/08-Герчук-Володимир/Lab_3/app.js
@@ -4,34 +4,38 @@
 // місцями перше та останнє слово, не змінивши довжини речення.
 // String object unchangable in JAVA
 
-function isString(variable) {
-  return typeof (variable) === 'string';
+export function isString(variable) {
+  return typeof variable === 'string';
 }
 
-class Text {
+export class Text {
   constructor(str) {
     if (isString(str)) {
       this.str = str;
-      // eslint-disable-next-line no-useless-escape
-      this.arr = this.str.replace(/[0-9]|[...]|[.,\/#!$%?\^&\*;:{}=\-_`~()@]/g, ' ').trim().split(' ');
+      this.arr = Text.createArr(str);
     } else {
       throw new Error(`${str} - not a string.\nExpected 'string' data type`);
     }
   }
 
+  static createArr(str) {
+    return str.replace(/[0-9]|[...]|[.,\/#!$%?\^&\*;:{}=\-_`~()@]/g, ' ').trim().split(' ');
+  }
+
   move() {
     if (this.arr.length < 2) {
-      console.error('Less than two words');
+      throw Error('Less than two words');
       return;
     }
     const lastEl = this.arr[this.arr.length - 1];
     const firstEl = this.arr[0];
-    // replace - replaces only first occurence
-    // todo fix lasEl replace
+    // TODO: fix lasEl replace
     this.replaced = this.str.replace(lastEl, firstEl).replace(firstEl, lastEl);
+    this.arr = Text.createArr(this.replaced);
+    return this.replaced;
   }
 }
 
-const newStr = new Text('/././.123__Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud Lorem ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate laborum esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est %*^%&^*laborum.123');
+const newStr = new Text('/././.123__Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud Lorem ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate laborum esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 newStr.move();
-console.log(newStr.replaced);
+// console.log(newStr.replaced);
---
+++ src/IO_02/08-Герчук-Володимир/Lab_3/app.test.js
@@ -0,0 +1,27 @@
+import {jest} from '@jest/globals';
+import { Text } from './app';
+
+const template = 'First simple string Last...';
+
+describe('Lab-3 => Words replacing', () => {
+  test('Check for the right value in str', () => {
+    const commonString = new Text(template);
+    expect(commonString.str).toBe(template);
+  });
+  test('Throw error with not a string', () => {
+    expect(() => new Text(123)).toThrow(/not a string/i);
+  });
+  test('Split a string on words, without punctuation marks', () => {
+    expect(Text.createArr(template)).toEqual(['First', 'simple', 'string', 'Last']);
+    expect(Text.createArr('Some.Text.Goes-here...')).toEqual(['Some', 'Text', 'Goes', 'here']);
+  });
+  test('Replace first and last words accordingly', () => {
+    const commonString = new Text(template);
+    expect(commonString.move()).toBe('Last simple string First...')
+    expect(commonString.move()).toMatch(/^first.*last/i);
+  });
+  test('Throw error on less than two words', () => {
+    const commonString = new Text('HereErrorComes');
+    expect(() => commonString.move()).toThrow(/less than two/i)
+  });
+});
