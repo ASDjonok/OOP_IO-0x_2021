d7fb130a1494bea9358d090aaea12f7f38e1d526
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-05-21 10:10:52 +0300

    add lab6

---
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab6/Dicotyledonous.java
@@ -0,0 +1,7 @@
+package IO_00._00_Алещенко_Олексій_Вадимович.lab6;
+
+public class Dicotyledonous extends Flower {
+    public Dicotyledonous(int freshness, int length) {
+        super(freshness, length);
+    }
+}
--- src/IO_00/_00_Алещенко_Олексій_Вадимович/lab6/Flower.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab6/Flower.java
@@ -1,4 +1,11 @@
 package IO_00._00_Алещенко_Олексій_Вадимович.lab6;
 
 public class Flower {
+    private int freshness;
+    private int length;
+
+    public Flower(int freshness, int length) {
+        this.freshness = freshness;
+        this.length = length;
+    }
 }
