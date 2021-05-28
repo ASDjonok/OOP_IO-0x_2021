bfafa0128600316fd764f0fcebd523e5a8f103c4
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-05-28 10:09:47 +0300

    add lab7

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/lab7/Main.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab7/Main.java
@@ -1,19 +1,27 @@
 package IO_00._00_Алещенко_Олексій_Вадимович.lab7;
 
-import IO_00._00_Алещенко_Олексій_Вадимович.lab6.Flower;
+import IO_00._00_Алещенко_Олексій_Вадимович.lab6.Cornflower;
 
 public class Main {
     public static void main(String[] args) {
-        MyFlowerCollection flowers = new MyFlowerCollection();
+        MyFlowerSet myFlowerSet = new MyFlowerSet();
+//        myFlowerSet.add(1);
+        System.out.println(myFlowerSet.size());
+        myFlowerSet.add(new Cornflower(1, 1));
+//        myFlowerSet.add("");
+        System.out.println(myFlowerSet.size());
+//        System.out.println(myFlowerSet);
+
+       /* MyFlowerCollection flowers = new MyFlowerCollection();
         MyGenericCollection<Flower> flowers1 = new MyGenericCollection<>();
-        flowers1.add(new Flower());
+//        flowers1.add(new Flower());
 //        flowers1.add(new String("123"));
-        flowers.add(new Flower());
+//        flowers.add(new Flower());
 //        flowers.add(new String("123"));
 
         MyGenericCollection<String> strings = new MyGenericCollection<>();
         strings.add(new String("123"));
-//        strings.add(new Flower());
+//        strings.add(new Flower());*/
 
     }
 }
---
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/lab7/Node.java
@@ -0,0 +1,12 @@
+package IO_00._00_Алещенко_Олексій_Вадимович.lab7;
+
+import IO_00._00_Алещенко_Олексій_Вадимович.lab6.Flower;
+
+public class Node {
+    private Flower flower;
+    private Node next;
+
+    public Node(Flower flower) {
+        this.flower = flower;
+    }
+}
