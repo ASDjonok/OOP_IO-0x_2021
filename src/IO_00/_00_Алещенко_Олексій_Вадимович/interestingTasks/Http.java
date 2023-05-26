f4106c43cf2b9740c1cd5ec79aa67ee1fb7d4cf8
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-05-26 20:07:39 +0300

    Update Http.java

--- src/IO_00/_00_Алещенко_Олексій_Вадимович/interestingTasks/Http.java
+++ src/IO_00/_00_Алещенко_Олексій_Вадимович/interestingTasks/Http.java
@@ -1,5 +1,5 @@
 package IO_00._00_Алещенко_Олексій_Вадимович.interestingTasks;
-
+// todo Що виведе програма?
 public class Http {
     public static void main(String[] args) {
         http://google.com
